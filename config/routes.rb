0e617d49c64d889c0dc35e42f01570067404850f
artifi <you@example.com>
2019-04-25 23:45:09 +0900

    cbuil pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'home/about'
+
+  get 'home/contact'
+
   #Restfulルーティング
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
