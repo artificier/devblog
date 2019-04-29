c09b9e1bcf31a1eb25417bd24852bf1ea35f6418
artifi <you@example.com>
2019-04-29 18:46:21 +0900

    Integrated inde action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,8 @@
 class PortfoliosController < ApplicationController
+  def index
+    #Calling the model inside of the controller
+	#available to the view
+    @portfolio_items = Portfolio.all
+  end
+
 end
