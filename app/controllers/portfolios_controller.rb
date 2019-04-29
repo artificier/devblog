610b914e330d467c26ec713eebf1ad6c52aa308b
artifi <you@example.com>
2019-04-29 20:24:57 +0900

    Integrated new and create actons for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,4 +5,24 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+   def new
+     @portfolio_item = Portfolio.new
+   end
+   
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+	    #format.htmlでブラウザで利用できるようにする。
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio is now live.' }
+        #上と同じ内容→format.html { redirect_to blog_path(@blog), notice: 'Blog was successfully created.' }
+        #format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        #format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
