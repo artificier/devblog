0793f0ad9cb8a42f306bf479e43a93d2fd9b12f2
artifi <you@example.com>
2019-05-11 20:21:13 +0900

    customize root

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   #rails g resource Portfolio title:string subtitle:string ....
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
 
-  get 'pages/contact'
+  #get 'pages/about'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
 
   get 'home/about'
 
   get 'home/contact'
-
+  
+  root to: 'pages#home'   
   #Restfulルーティング
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
