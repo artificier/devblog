ea6991a702834e474aa456dee206995ae3cedb56
artifi <you@example.com>
2019-06-01 17:32:43 +0900

    Integerated database relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190517102319) do
+ActiveRecord::Schema.define(version: 20190601081548) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20190517102319) do
     t.string "slug"
     t.integer "stat", default: 0
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -47,6 +49,12 @@ ActiveRecord::Schema.define(version: 20190517102319) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
