3de9d383fef67d076c587a9acd0739f9c0fd7859
artifi <you@example.com>
2019-07-03 19:17:40 +0900

    Refactored stron params for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,7 @@ class PortfoliosController < ApplicationController
    
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -40,7 +40,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 	
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -65,5 +65,15 @@ class PortfoliosController < ApplicationController
 	  format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
 	end
 	
+  end
+  
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+									  :subtitle,
+									  :body,
+									  technologies_attributes: [:name]
+									  )
   end
 end
