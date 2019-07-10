3b1d097ea23c58543a44a502c16a933c183d6ca1
artifi <you@example.com>
2019-07-10 15:38:07 +0900

    Implemented content tag helpler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def sample_helper
-    "<p>My Helper</p>".html_safe
-  end
-  
   def login_helper
     if current_user.is_a?(User)
 	  link_to "Logout", destroy_user_session_path, method: :delete
@@ -11,5 +7,12 @@ module ApplicationHelper
 	  "<br>".html_safe +
 	  (link_to "Login", new_user_session_path)
 	end
+  end
+  
+  def source_helper(layout_name)
+    if session[:source]
+	  greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
 end
